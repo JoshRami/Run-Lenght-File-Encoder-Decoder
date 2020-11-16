d6f23a9842d2ebbd9c3d31d2ee530e1c5bb7ad57
Josue <qjismael000@gmail.com>
2020-11-16 12:46:08 -0600

    Add error cases in rle functions
    Add test to rle module
    Split functions logic in rle module

--- src/rle/rle.test.ts
+++ src/rle/rle.test.ts
@@ -1,6 +1,47 @@
 import * as rle from './rle';
 
-describe('Decode and encode same data', () => {
+describe('handling errors regarding bad data format', () => {
+  it('should throw error when space found between text', () => {
+    expect(() => {
+      rle.decode('3A3B 4D');
+    }).toThrow();
+  });
+  it('should throw error when text to encode not contains just only letters', () => {
+    expect(() => {
+      rle.encode('ABD1');
+    }).toThrow();
+  });
+  it('should throw error when text to decode not contains just only letters & numbers', () => {
+    expect(() => {
+      rle.decode('ABD1$');
+    }).toThrow();
+  });
+  it('should trim string and not throw eror', () => {
+    expect(rle.decode(' 3A3B4D ')).toBe('AAABBBDDDD');
+  });
+});
+describe('encoding', () => {
+  it('should encode as expected', () => {
+    expect(rle.encode('QQQTTTYYY')).toBe('3Q3T3Y');
+  });
+  it('should encode uppercase and lowercase well', () => {
+    expect(rle.encode('ABBCCCdddDDD')).toBe('1A2B3C3d3D');
+  });
+});
+describe('decoding', () => {
+  it('should decode as expected', () => {
+    expect(rle.decode('3Q3T3Y')).toBe('QQQTTTYYY');
+  });
+  it('should decode uppercase and lowercase well', () => {
+    expect(rle.decode('1A2B3C3d3D')).toBe('ABBCCCdddDDD');
+  });
+  it('should throw an error if bad pattern is found while decoding', () => {
+    expect(() => {
+      rle.decode('1BB');
+    }).toThrow();
+  });
+});
+describe('decode and encode same data', () => {
   it('should encode a decode the same data', () => {
     const encoded = rle.encode(
       'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW',
@@ -10,20 +51,5 @@ describe('Decode and encode same data', () => {
     expect(decoded).toBe(
       'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW',
     );
-  });
-});
-describe('Decode data', () => {
-  it('should throw an error if empty space while decoding', () => {
-    expect(() => {
-      rle.decode('3A3B 4D');
-    }).toThrow();
-  });
-  it('should throw error when space found between text while decoding', () => {
-    expect(() => {
-      rle.decode('3A3B 4D');
-    }).toThrow();
-  });
-  it('should trim string and not throw eror', () => {
-    expect(rle.decode(' 3A3B4D ')).toBe('AAABBBDDDD');
   });
 });
--- src/rle/rle.ts
+++ src/rle/rle.ts
@@ -1,10 +1,10 @@
 /**
  * Encodes a chunk of data into run length encoding format
- * @param {data} string - Receive chunk of data, data can have spaces
+ * @param {data} string - Receive chunk of data, no space and only letters
  * @returns {string} - return rle encoded data
  */
 export const encode = (data: string): string => {
-  const sanitizedData: string[] = data.trim().split('');
+  const sanitizedData: string[] = getSanitizedData(data, 'encode').split('');
 
   let repetitions = 1;
   return sanitizedData.reduce(
@@ -24,35 +24,59 @@ export const encode = (data: string): string => {
 
 /**
  * Decodes run length encoding chunk of data
- * @param {data} string - Receive rle encoded data, data to decode
- *                        cannot have numebers or special char
+ * @param {data} string - rle encoded data, no espace and only letter & number
  * @returns {string} - return decoded data
  */
 export const decode = (data: string): string => {
-  const space = ' ';
-  const splitRegex = new RegExp(/[a-zA-Z]+|[0-9]+(?:\.[0-9]+)?|\.[0-9]+/g);
-  const sanitizedData = data.trim();
+  const sanitizedData = getSanitizedData(data, 'decode');
 
-  if (sanitizedData.includes(space)) {
-    throw new Error('rle: encoded text cannot include space');
-  }
+  const splitRegex = new RegExp(/[a-zA-Z]+|[0-9]+(?:\.[0-9]+)?|\.[0-9]+/g);
   const encodedUnits: string[] = sanitizedData.match(splitRegex);
 
-  const encodedData = encodedUnits.reduce(
-    (acc: string, unit: string, index: number) => {
-      const letter = encodedUnits[index + 1];
-      const repetitions = Number(unit);
-      if (index === encodedUnits.length - 1 || index % 2 === 1) {
-        return acc;
-      }
-      if (letter.length > 1) {
-        throw new Error('rle: Bad encoded format');
-      }
-      const encodedUnit = letter.repeat(repetitions);
+  return encodedUnits.reduce((acc: string, unit: string, index: number) => {
+    if (index === encodedUnits.length - 1 || index % 2 === 1) {
+      return acc;
+    }
+    const letter = encodedUnits[index + 1];
+    if (letter.length > 1 || isNaN(Number(unit))) {
+      throw new Error('rle: Bad encoded format');
+    }
 
-      return acc.concat(encodedUnit);
-    },
-    '',
-  );
-  return !encodedData ? sanitizedData : encodedData;
+    const repetitions = Number(unit);
+    const encodedUnit = letter.repeat(repetitions);
+    return acc.concat(encodedUnit);
+  }, '');
+};
+
+type option = 'encode' | 'decode';
+
+/**
+ * Trim incoming data and check for invalid format depending the option
+ * @param {data} string - Incoming data from input file
+ * @param {action} option - Option type wich can be decode or decode
+ *                          depending on option passed, especific validation will check
+ * @returns {string} - return sanitized data
+ */
+const getSanitizedData = (data: string, action: option): string => {
+  const sanitizedData = data.trim();
+  const space = ' ';
+  if (sanitizedData.includes(space)) {
+    throw new Error('rle: text cannot include space between');
+  }
+
+  if (action === 'encode') {
+    const onlyLetters = new RegExp(/^[a-zA-Z]+$/);
+    if (!onlyLetters.test(sanitizedData)) {
+      throw new Error('rle: text can includes just letters while encoding');
+    }
+  } else if (action === 'decode') {
+    const onlyLetterNumber = new RegExp(/^[0-9a-zA-Z]+$/);
+    if (!onlyLetterNumber.test(sanitizedData)) {
+      throw new Error(
+        'rle: text can includes just letters & numbers while decoding',
+      );
+    }
+  }
+
+  return sanitizedData;
 };
